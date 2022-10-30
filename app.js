bbbfdec2b52dd05fd2ae7a2f3f72ca648d8abfef
imranhossain-code <imranfahim552@gmail.com>
2022-10-31 00:05:12 +0600

    add cors

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express();
 const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(require("./router/auth"));
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-}
 
 app.get("/", async (req, res) => {
   res.send(`Ishop backend 22 ${PORT}`);
