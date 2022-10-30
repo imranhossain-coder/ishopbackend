375ed8b36eb5b6d48765d470ace8d2b98e4073f4
imranhossain-code <imranfahim552@gmail.com>
2022-10-30 23:26:51 +0600

    knowing Port

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
-const cookieParser =require('cookie-parser');
+const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
 
 const PORT = process.env.PORT || 5000;
 
-
-app.use(cookieParser()) ;
+app.use(cookieParser());
 app.use(express.json());
 app.use(require("./router/auth"));
 
-if(process.env.NODE_ENV == "production"){
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 }
 
+app.get("/", async (req, res) => {
+  res.send(`Ishop backend 22 ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running in port number ${PORT}`);
